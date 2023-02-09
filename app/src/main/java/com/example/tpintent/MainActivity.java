1b0f468232cb973a9323ca235b5118d5ccf9a696
melekabbassi <abbassimelek@gmail.com>
2023-02-09 10:58:15 +0100

    added functions for buttons 3,4,5,6

--- app/src/main/java/com/example/tpintent/MainActivity.java
+++ app/src/main/java/com/example/tpintent/MainActivity.java
@@ -38,6 +38,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 intent.putExtra("data", editText.getText().toString());
                 startActivity(intent);
                 break;
+            case R.id.button3:
+                // navigate to Google.com
+                intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(android.net.Uri.parse("http://www.google.com"));
+                startActivity(intent);
+                break;
+            case R.id.button4:
+                // Application Settings
+                intent = new Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                intent.setData(android.net.Uri.parse("package:" + getPackageName()));
+                startActivity(intent);
+                break;
+            case R.id.button5:
+                // make a call
+                intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(android.net.Uri.parse("tel:123456789"));
+                startActivity(intent);
+                break;
+            case R.id.button6:
+                // send an email
+                intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"aaaa", "bbbb"});
+                intent.putExtra(Intent.EXTRA_SUBJECT, "subject");
+                intent.putExtra(Intent.EXTRA_TEXT, "text");
+                startActivity(intent);
+                break;
         }
     }
 }
